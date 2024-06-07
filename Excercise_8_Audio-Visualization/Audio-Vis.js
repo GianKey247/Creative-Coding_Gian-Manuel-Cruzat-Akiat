5ae95aa362c0c314461ee5e8f349839247f75b71
GianKey247 <gianakiat@gmail.com>
2024-06-07 11:00:46 +0400

    updating

--- Excercise_8_Audio-Visualization/Audio-Vis.js
+++ Excercise_8_Audio-Visualization/Audio-Vis.js
@@ -1,84 +1,67 @@
+var mySound, fft, bgImage;
+
+let numBars = 128;
+let barWidth, barHeight;
+let barSpacing = 1;
+
 function preload() {
-    font = loadFont('AmaticSC-Regular.ttf');
-  }
-  function Mover(){
-    this.location = createVector(random(width), random(height));
-    this.velocity = createVector(random(-2, 2), random(-2, 2));
-    this.acceleration = createVector(-0.001,0.01); // 2 a constant accelleration, for now
-    this.topspeed = 20; //2
+  mySound = loadSound('KEYGEN CHURCH - Tenebre Rosso Sangue (Calm Loop).mp3');
+  bgImage = loadImage('WAIT_OF_THE_WORLD.png');
+}
+
+function setup() {
+  createCanvas(windowWidth, 1080);
   
-    this.update = function() {
-      // particles going to mouses location
-      let mouse = createVector(mouseX,mouseY);
-      let dir = p5.Vector.sub(mouse, this.location);
-      dir.normalize();
-      dir.mult(0.5);
-      this.acceleration = dir;
+  // Create a new FFT analysis
+  fft = new p5.FFT();
   
-      //acceleration of particle
-      this.velocity.add(this.acceleration);
-      this.velocity.limit(this.topspeed);
+  mySound.setVolume(0.4);
+  mySound.play();
   
-      //changing location
-      this.location.add(this.velocity);
-    }
+  // Calculate the Bars width
+  barWidth = width / numBars - barSpacing;
+}
+
+function draw() {
+  // Draw the background image with proper scaling
+  let imgAspect = bgImage.width / bgImage.height;
+  let canvasAspect = width / height;
   
-    this.display = function() {
-      //display particle
-      stroke(44,59,82)
-      strokeWeight(2)
-      fill(255,255,255)
-      ellipse(this.location.x,this.location.y,4.5,4.5);
-    }
-      //particle will go to other side of screen when it touches the edges
-    this.checkEdges = function() {
-      if (this.location.x > width) {
-        this.location.x = 0;
-      } else if (this.location.x < 0) {
-        this.location.x = width;
-      }
-  
-      if (this.location.y > height) {
-        this.location.y = 0;
-      } else if (this.location.y < 0) {
-        this.location.y = height;
-      }
-    }
+  let drawWidth, drawHeight;
+  if (imgAspect > canvasAspect) {
+    drawWidth = width;
+    drawHeight = width / imgAspect;
+  } else {
+    drawWidth = height * imgAspect;
+    drawHeight = height;
   }
   
-  let mover;
-  
-  let movers = [];
-  
-  function setup() {
-    createCanvas(windowWidth, windowHeight);
-    mover = new Mover();
-    //create particles a X amount of times
-    for (let i = 0; i < 2000; i++) {
-      movers[i] = new Mover();
-    }
-  
+  image(bgImage, (width - drawWidth) / 2, (height - drawHeight) / 2, drawWidth, drawHeight);
+
+  stroke(0, 0, 0);
+  strokeWeight(1);
+
+  // Draws the bars
+  let spectrum = fft.analyze();
+  for (let i = 0; i < numBars; i++) {
+    barHeight = map(spectrum[i], 0, 255, 0, height) * 0.6; // Reduce amplitude
+    fill(lerpColor(color('rgb(255,0,0)'), color('#FF5800'), i / numBars));
+    rect(i * (barWidth + barSpacing), height - barHeight, barWidth, barHeight);
   }
-  
-  function draw() {
-    //Bathspa Text
-    background(33,37,62);
-    textAlign(CENTER)
-    textSize(120);
-    fill(255);
-    textFont(font);
-    stroke(0, 0, 0);
-    strokeWeight(4);
-    text('Welcome to BathSpa University', windowWidth/3.4, windowHeight/2, 685);
-    
-    mover.update();
-  
-    mover.checkEdges();
-  
-    for (let i = 0; i < movers.length; i++) {
-      //updates each particle
-      movers[i].update();
-      movers[i].checkEdges();
-      movers[i].display();
-    }
+
+  let waveform = fft.waveform();
+
+  // Draws the waveform
+  noFill();
+  beginShape();
+  stroke(0,0,0);
+  strokeWeight(5);
+
+  for (let i = 0; i < waveform.length; i++) {
+    let x = map(i, 0, waveform.length, 0, width);
+    let y = map(waveform[i], -1, 1, 0, height) + 450; // Move waveform down
+    vertex(x, y);
   }
+
+  endShape();
+}
